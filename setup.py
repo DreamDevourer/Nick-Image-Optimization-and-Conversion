5be796c993e38e0161dda28192f1ce133f944d23
Nicolas Mendes <nicolasmendes_developer@outlook.com>
2021-09-23 20:42:45 -0300

    Darwin build binaries

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""
+This is the setup to generate MacOS package.
+Troubleshooting: sudo chown -R $(whoami) .
+"""
+
+from setuptools import setup
+
+APP = ['Nick - Image Optimization and Conversion.py']
+DATA_FILES = ['images', 'assets', 'images/backup']
+OPTIONS = {}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
