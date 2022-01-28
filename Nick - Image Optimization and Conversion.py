2dd64cfc19f9ed428aef1495461efe989b0da0a9
Nicolas Mendes <nicolasmendes_developer@outlook.com>
2022-01-28 14:20:07 -0300

    Fixing message box inside for loop

--- Nick - Image Optimization and Conversion.py
+++ Nick - Image Optimization and Conversion.py
@@ -85,68 +85,41 @@ canvas = Canvas(
     width=980,
     bd=0,
     highlightthickness=0,
-    relief="ridge"
+    relief="ridge",
 )
 
 # Buttons and Labels
 canvas.place(x=0, y=0)
-image_image_1 = PhotoImage(
-    file=relative_to_assets("image_1.png"))
-image_1 = canvas.create_image(
-    95.0,
-    290.0,
-    image=image_image_1
-)
+image_image_1 = PhotoImage(file=relative_to_assets("image_1.png"))
+image_1 = canvas.create_image(95.0, 290.0, image=image_image_1)
 
 # Make magic button ( Aka optimizing and conversion functions )
-button_image_1 = PhotoImage(
-    file=relative_to_assets("button_1.png"))
+button_image_1 = PhotoImage(file=relative_to_assets("button_1.png"))
 button_1 = Button(
     image=button_image_1,
     borderwidth=0,
     highlightthickness=0,
     command=lambda: optimizationFunction(),
-    relief="flat"
+    relief="flat",
 )
-button_1.place(
-    x=28.0,
-    y=502.0,
-    width=135.0,
-    height=46.0
-)
+button_1.place(x=28.0, y=502.0, width=135.0, height=46.0)
 
-image_image_2 = PhotoImage(
-    file=relative_to_assets("image_2.png"))
-image_2 = canvas.create_image(
-    577.0,
-    348.0,
-    image=image_image_2
-)
+image_image_2 = PhotoImage(file=relative_to_assets("image_2.png"))
+image_2 = canvas.create_image(577.0, 348.0, image=image_image_2)
 
-image_image_3 = PhotoImage(
-    file=relative_to_assets("image_3.png"))
-image_3 = canvas.create_image(
-    577.0,
-    64.0,
-    image=image_image_3
-)
+image_image_3 = PhotoImage(file=relative_to_assets("image_3.png"))
+image_3 = canvas.create_image(577.0, 64.0, image=image_image_3)
 
 # Browse Button ( Aka selecting files )
-button_image_2 = PhotoImage(
-    file=relative_to_assets("button_2.png"))
+button_image_2 = PhotoImage(file=relative_to_assets("button_2.png"))
 button_2 = Button(
     image=button_image_2,
     borderwidth=0,
     highlightthickness=0,
     command=lambda: loadFolderForImg(),
-    relief="flat"
+    relief="flat",
 )
-button_2.place(
-    x=812.0,
-    y=41.0,
-    width=135.0,
-    height=46.0
-)
+button_2.place(x=812.0, y=41.0, width=135.0, height=46.0)
 
 # 🔖 Load GUI files defs
 
@@ -163,26 +136,12 @@ def loadFolderForImg():
 # Entry to load files
 entry_DefPath = StringVar()
 entry_DefPath.set(str(IMAGES_PATH))
-entry_image_1 = PhotoImage(
-    file=relative_to_assets("entry_1.png"))
-entry_bg_1 = canvas.create_image(
-    567.5,
-    74.0,
-    image=entry_image_1
-)
+entry_image_1 = PhotoImage(file=relative_to_assets("entry_1.png"))
+entry_bg_1 = canvas.create_image(567.5, 74.0, image=entry_image_1)
 entry_1 = Entry(
-    bd=0,
-    bg="#FFFFFF",
-    fg="#000000",
-    textvariable=entry_DefPath,
-    highlightthickness=0
+    bd=0, bg="#FFFFFF", fg="#000000", textvariable=entry_DefPath, highlightthickness=0
 )
-entry_1.place(
-    x=364.0,
-    y=41.0,
-    width=407.0,
-    height=44.0
-)
+entry_1.place(x=364.0, y=41.0, width=407.0, height=44.0)
 
 # Dummy label ( Don't touch )
 canvas.create_text(
@@ -191,7 +150,7 @@ canvas.create_text(
     anchor="nw",
     text="Select Folder",
     fill="#000000",
-    font=("Mulish Regular", 18 * -1)
+    font=("Mulish Regular", 18 * -1),
 )
 
 # Listbox to show files loaded rootWindow
@@ -209,7 +168,12 @@ list_items = Listbox(
 list_items.place(relx=0.5, rely=0.5, anchor="center")
 
 for file in files:
-    if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".gif"):
+    if (
+        file.endswith(".png")
+        or file.endswith(".jpg")
+        or file.endswith(".jpeg")
+        or file.endswith(".gif")
+    ):
         list_items.insert(END, file)
 
 canvas.create_text(
@@ -218,16 +182,11 @@ canvas.create_text(
     anchor="nw",
     text="Images found inside the folder",
     fill="#000000",
-    font=("Mulish Regular", 16 * -1)
+    font=("Mulish Regular", 16 * -1),
 )
 
-image_image_4 = PhotoImage(
-    file=relative_to_assets("image_4.png"))
-image_4 = canvas.create_image(
-    226.0,
-    165.0,
-    image=image_image_4
-)
+image_image_4 = PhotoImage(file=relative_to_assets("image_4.png"))
+image_4 = canvas.create_image(226.0, 165.0, image=image_image_4)
 
 # Logo text ( Don't touch )
 canvas.create_text(
@@ -236,7 +195,7 @@ canvas.create_text(
     anchor="nw",
     text="I.O.C",
     fill="#FFFFFF",
-    font=("Mulish Bold", 36 * -1)
+    font=("Mulish Bold", 36 * -1),
 )
 
 """
@@ -283,13 +242,8 @@ reduceByHalfChck.deselect()
 reduceByHalfChck.place(relx=0.03, rely=0.7, anchor="nw")
 
 # Logo Icon
-image_image_6 = PhotoImage(
-    file=relative_to_assets("image_6.png"))
-image_6 = canvas.create_image(
-    95.0,
-    64.0,
-    image=image_image_6
-)
+image_image_6 = PhotoImage(file=relative_to_assets("image_6.png"))
+image_6 = canvas.create_image(95.0, 64.0, image=image_image_6)
 
 # ⚙️ Logic and Defs
 
@@ -312,12 +266,19 @@ def updateListbox():
     files = updateFilesFound
 
     for file in files:
-        if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".gif"):
+        if (
+            file.endswith(".png")
+            or file.endswith(".jpg")
+            or file.endswith(".jpeg")
+            or file.endswith(".gif")
+        ):
             list_items.delete(0, END)
             list_items.insert(END, file)
             print(f"Found valid images in {folderImgs} with {file}.")
 
+
 # =========== 🧬 Optimization Functions ===========
+
 
 def optimizationFunction():
     global reduceByHalf
@@ -325,20 +286,23 @@ def optimizationFunction():
 
     updateListbox()
 
-    print(
-        f"These are all of the files in our current working directory: {files}")
+    print(f"These are all of the files in our current working directory: {files}")
     confirmFiles = "y"
     confirmDownRes = IntVar()
     confirmDownRes = reduceByHalf.get()
 
     if confirmFiles == "y":
         for file in files:
-            if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".gif"):
+            if (
+                file.endswith(".png")
+                or file.endswith(".jpg")
+                or file.endswith(".jpeg")
+                or file.endswith(".gif")
+            ):
 
                 # Backup operation
                 shutil.copy(
-                    f"{folderImgs}/{file}",
-                    f"{folderImgs}/backup/_backup_{file}"
+                    f"{folderImgs}/{file}", f"{folderImgs}/backup/_backup_{file}"
                 )
                 print(f"Copying {file} to backup folder")
 
@@ -355,71 +319,100 @@ def optimizationFunction():
                 # If confirmDownRes = 1, imgWidth and imgHeight are larger than 1366x768 reduce the resolution by half.
                 if imgWidth > 1366 and imgHeight > 768 and confirmDownRes == 1:
                     imgOptimize = imgOptimize.resize(
-                        (int(imgWidth / 2), int(imgHeight / 2)), PIL.Image.ANTIALIAS)
+                        (int(imgWidth / 2), int(imgHeight / 2)), PIL.Image.ANTIALIAS
+                    )
                     print(f"Reducing image resolution by half of {file}")
                     print(f"Optimized {file}")
 
                     if file.endswith(".png"):
-                        imgOptimize.save(str(relative_to_images(
-                            str(file))), optimize=True, quality=70)
+                        imgOptimize.save(
+                            str(relative_to_images(str(file))),
+                            optimize=True,
+                            quality=70,
+                        )
                         convertionFunction()
-                    if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".gif"):
-                        imgOptimize.save(str(relative_to_images(
-                            str(file))), optimize=True, quality=80)
+                    if (
+                        file.endswith(".jpg")
+                        or file.endswith(".jpeg")
+                        or file.endswith(".gif")
+                    ):
+                        imgOptimize.save(
+                            str(relative_to_images(str(file))),
+                            optimize=True,
+                            quality=80,
+                        )
                         convertionFunction()
 
                 # If user don't want to reduce image resolution by half.
                 else:
-                    print(
-                        f"Image: {file}, is not larger enough to reduce resolution.")
+                    print(f"Image: {file}, is not larger enough to reduce resolution.")
 
                     print(f"Performing standard optimization on {file}")
                     if file.endswith(".png"):
-                        imgOptimize.save(str(relative_to_images(
-                            str(file))), optimize=True, quality=70)
+                        imgOptimize.save(
+                            str(relative_to_images(str(file))),
+                            optimize=True,
+                            quality=70,
+                        )
                         convertionFunction()
-                    if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".gif"):
-                        imgOptimize.save(str(relative_to_images(
-                            str(file))), optimize=True, quality=80)
+                    if (
+                        file.endswith(".jpg")
+                        or file.endswith(".jpeg")
+                        or file.endswith(".gif")
+                    ):
+                        imgOptimize.save(
+                            str(relative_to_images(str(file))),
+                            optimize=True,
+                            quality=80,
+                        )
                         convertionFunction()
 
                 print(f"{file} optimized!")
 
             else:
                 print(
-                    f"No valid files found in {folderImgs} with {file}... Checking again.")
+                    f"No valid files found in {folderImgs} with {file}... Checking again."
+                )
                 updateListbox()
     else:
         print("Exiting...")
         sys.exit()
 
+
 # =========== 🎭 Convertion Functions ===========
 
 
 def convertionFunction():
-    print(
-        f"These are all of the files in our current working directory: {files}")
+    print(f"These are all of the files in our current working directory: {files}")
 
     confirmFiles = "y"
 
     if confirmFiles == "y":
         for file in files:
-            if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".gif"):
+            if (
+                file.endswith(".png")
+                or file.endswith(".jpg")
+                or file.endswith(".jpeg")
+                or file.endswith(".gif")
+            ):
                 print(f"Converting {file} to WebP")
                 loadImg = Image.open(relative_to_images(str(file)))
-                loadImg.save(str(relative_to_images(str(file))) +
-                             ".webp", "WEBP", quality=80)
+                loadImg.save(
+                    str(relative_to_images(str(file))) + ".webp", "WEBP", quality=80
+                )
                 print(f"{file} converted to WebP")
                 # Open images folder in default OS file manager.
-                subprocess.Popen(
-                    ["open", "-R", folderImgs])
+                subprocess.Popen(["open", "-R", folderImgs])
                 print(f"Opening {file} in Finder")
-                # Show a message window with "Optimization and conversion completed!"
-                messagebox.showinfo(
-                    "Optimization and conversion completed! \n", "All files have been optimized and converted to WebP!")
                 print("All files have been optimized and converted to WebP!")
             else:
                 print(f"{file} is not a PNG or JPG, skipping...")
+
+        # Show a message window with "Optimization and conversion completed!"
+        messagebox.showinfo(
+            "Optimization and conversion completed! \n",
+            "All files have been optimized and converted to WebP!",
+        )
     else:
         print("Exiting...")
         sys.exit()
